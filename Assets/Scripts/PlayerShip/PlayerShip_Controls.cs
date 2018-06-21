949a8a0e6457c1740862e395a6ec705eb802309c
Remlik <Remlikmg@comcast.net>
2018-06-21 18:50:27 -0400

    Created PlayerShip movement and control scripts and implemented basic movement

---
+++ Assets/Scripts/PlayerShip/PlayerShip_Controls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip_Controls : MonoBehaviour {
+
+    private PlayerShip_Move move;
+
+	// Use this for initialization
+	void Start () {
+        move = GetComponent<PlayerShip_Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        move.Boost(Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/PlayerShip/PlayerShip_Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip_Move : MonoBehaviour {
+
+    public float maxTiltAmount;
+    public float maxBoostStrength;
+    public float autoDampenRate;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+    public void Boost(float amnt)
+    {
+        if (amnt == 0)
+        {
+            rb.velocity = rb.velocity * autoDampenRate * Time.fixedDeltaTime;
+        } else
+        {
+            rb.AddForce(transform.right * amnt * maxBoostStrength);
+        }
+        Tilt(amnt);
+    }
+
+    private void Tilt(float amnt)
+    {
+        transform.rotation = Quaternion.Euler(0f, -maxTiltAmount * amnt, 0f);
+    }
+}
