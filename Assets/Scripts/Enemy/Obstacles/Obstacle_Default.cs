b1bdfd16abfce90c10759fad4f1338cf0b23790e
Remlik <Remlikmg@comcast.net>
2018-06-21 22:18:52 -0400

    Created freamework for obstacles and basic obstacle

---
+++ Assets/Scripts/Enemy/Obstacles/Obstacle_Default.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Default : MonoBehaviour, IObstacle {
+
+    public float speed;
+    public float timeAllowedToExist;
+    public float timeExisted = 0f;
+
+    private Rigidbody rb;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeExisted += Time.deltaTime;
+        if (timeExisted >= timeAllowedToExist)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void CollideWithPlayer()
+    {
+        gameObject.SetActive(false);
+    }
+}
