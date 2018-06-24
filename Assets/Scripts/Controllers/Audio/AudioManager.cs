425602083852a965cb1b5fd5d066e07f4d68037f
Remlik <Remlikmg@comcast.net>
2018-06-24 00:46:44 -0400

    Setup AudioManager and made the shield pulsate

---
+++ Assets/Scripts/Controllers/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager _sharedInstance;
+
+    public AudioMixer audioMixer;
+    public Sound[] sounds;
+    
+	void Awake () {
+        
+        if (_sharedInstance == null)
+        {
+            _sharedInstance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+		foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.playOnAwake = s.playOnAwake;
+            s.source.loop = s.loop;
+        }
+	}
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.Play();
+        } else
+        {
+            Debug.LogWarning("Sound " + name + " was not found");
+        }
+    }
+}
